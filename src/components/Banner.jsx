6c8d1acb221548abad04cb014648ad3888e0e490
VU DUY HAI <duyhai121120@gmail.com>
2024-10-30 02:52:15 +0900

    Add Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,10 @@
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+        <div className="w-[full] h-[600px] bg-banner bg-center bg-no-repeat bg-cover relative">
+            <div className="absolute w-full h-full top-0 left-0 bg-black opacity-30" />
+            Banner
+        </div>
+    );
 };
 
 export default Banner;
