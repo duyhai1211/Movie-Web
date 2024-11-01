e088ffd64fb707dd521633a46651700fb4511045
VU DUY HAI <duyhai121120@gmail.com>
2024-11-02 03:20:03 +0900

    Add API + Fixbug  dấu`` cayvcl

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import MovieList from "./components/MovieList";
 
 function App() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                },
+            };
+            const url = "https://api.themoviedb.org/3/movie/popular?language=vi&page=1";
+
+            const response = await fetch(url, options);
+            const data = await response.json();
+
+            console.log(data);
+        }
+
+        fetchMovie();
+    }, []);
 
     return (
         <>
-            <div className= "bg-black pb-10">
+            <div className="bg-black pb-10">
                 <Header />
                 <Banner />
-                <MovieList title={"Phim Hot"}/>
-                <MovieList title={"Phim Đề Cử"}/>
+                <MovieList title={"Phim Hot"} />
+                <MovieList title={"Phim Đề Cử"} />
             </div>
         </>
     );
