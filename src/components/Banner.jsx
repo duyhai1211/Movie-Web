bdc55e2c992b791d165ed60374232a84b8c2d7d4
VU DUY HAI <duyhai121120@gmail.com>
2024-11-01 23:54:56 +0900

    Add Banner 2 + Fix bug

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,21 +3,28 @@ import IconRatingHalf from "../assets/rating-half.png";
 
 const Banner = () => {
     return (
-        <div className="w-[full] h-[600px] bg-banner bg-center bg-no-repeat bg-cover relative">
+        <div className="w-full h-[600px] bg-banner bg-center bg-no-repeat bg-cover relative">
             <div className="absolute w-full h-full top-0 left-0 bg-black opacity-30" />
-            <div className="w-full h-full fle items-center justify-center space-x-[30] p-4 relative z-20"></div>
-            <div className="flex flex-col space-y-5 itmes-baselines">
-                <p className="text-white bg-gradient-to-r from-red-600 to white py-1 px-3">TV Show</p>
-                <div className="flex flex-col space-y-4">
-                    <h2 className="text-white text-3x1">Nghe nói em thích tôi</h2>
-                    <div className="flex items-center space-x-3">
-                        <img src={IconRating} alt="rating" className="w-8 h-8" />
-                        <img src={IconRating} alt="rating" className="w-8 h-8" />
-                        <img src={IconRating} alt="rating" className="w-8 h-8" />
-                        <img src={IconRating} alt="rating" className="w-8 h-8" />
-                        <img src={IconRatingHalf} alt="rating" className="w-8 h-8" />
+            <div className="w-full h-full flex items-center justify-center space-x-[30px] p-4 relative z-20">
+                <div className="flex flex-col space-y-5 items-baseline w-[50%]">
+                    <p className="text-white bg-gradient-to-r from-red-600 to-white text-md py-2 px-3">TV Show</p>
+                    <div className="flex flex-col space-y-4">
+                        <h2 className="text-white text-3x1">Tôi yêu em T</h2>
+                        <div className="flex items-center space-x-3">
+                            <img src={IconRating} alt="rating" className="w-8 h-8" />
+                            <img src={IconRating} alt="rating" className="w-8 h-8" />
+                            <img src={IconRating} alt="rating" className="w-8 h-8" />
+                            <img src={IconRating} alt="rating" className="w-8 h-8" />
+                            <img src={IconRatingHalf} alt="rating" className="w-8 h-8" />
+                        </div>
+                        <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus adipisci laboriosam odit, quis quam quibusdam officia natus tempora veniam, voluptate iste inventore quae labore. Temporibus nisi quod minus numquam rem!</p>
+                        <div className="flex items-center space-x-4">
+                            <button className="p-3 text-white bg-black font-bold text-sm">Chi Tiết</button>
+                            <button className="p-3 text-white bg-red-600 font-bold text-sm">Xem Phim</button>
+                        </div>
                     </div>
                 </div>
+                <div className="w-[50%]">image</div>
             </div>
         </div>
     );
