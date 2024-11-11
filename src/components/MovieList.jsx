13a7d56d2c2242bec3534bc66af34d60972c42fc
VU DUY HAI <duyhai121120@gmail.com>
2024-11-12 03:13:30 +0900

    Add Carousel + Modal Youtube

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Modal from "react-modal";
+import YouTube from "react-youtube";
+
+
+const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+        autoplay: 1,
+    },
+};
 
 const responsive = {
     superLargeDesktop: {
-        // the naming can be any, depends on you.
         breakpoint: { max: 4000, min: 3000 },
         items: 10,
     },
@@ -23,6 +34,33 @@ const responsive = {
 };
 
 const MovieList = ({ title, data }) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [trailerKey, setTrailerKey] = useState("");
+
+    const handleTrailer = async (id) => {
+        setTrailerKey("");
+        try {
+            const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                },
+            };
+
+            const response = await fetch(url, options);
+            const data = await response.json();
+            if (data.results && data.results.length > 0) {
+                setTrailerKey(data.results[0].key);
+                setModalIsOpen(true);
+            }
+        } catch (error) {
+            setModalIsOpen(false);
+            console.log(error);
+        }
+    };
+
     return (
         <div className="text-white p-10 mb-10">
             <h2 className="uppercase text-xl mb-4">{title}</h2>
@@ -35,13 +73,12 @@ const MovieList = ({ title, data }) => {
                         <div
                             key={item.id}
                             className="w-[200px] h-[300px] relative group"
+                            onClick={() => handleTrailer(item.id)}
                         >
                             <div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
                                 <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
                                 <img
-                                    src={`${import.meta.env.VITE_IMG_URL}${
-                                        item.poster_path
-                                    }`}
+                                    src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`}
                                     alt={item.title}
                                     className="w-full h-full object-cover"
                                 />
@@ -54,6 +91,30 @@ const MovieList = ({ title, data }) => {
                         </div>
                     ))}
             </Carousel>
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setModalIsOpen(false)}
+                style={{
+                    overlay: {
+                        position: "fixed",
+                        zIndex: 9999,
+                    },
+                    content: {
+                        top: "50%",
+                        left: "50%",
+                        right: "auto",
+                        bottom: "auto",
+                        marginRight: "-50%",
+                        transform: "translate(-50%, -50%)",
+                    },
+                }}
+                contentLabel="Trailer Modal"
+            >
+                {trailerKey && (
+                    <YouTube videoId={trailerKey} opts={opts} />
+                )}
+            </Modal>
         </div>
     );
 };
