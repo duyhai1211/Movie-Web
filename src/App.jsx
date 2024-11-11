6c747dce57ee2288297b6819a28e4c4d65eb8d5d
VU DUY HAI <duyhai121120@gmail.com>
2024-11-12 01:10:43 +0900

    Update MovieList Final

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MovieList from "./components/MovieList";
 
 function App() {
     const [movie, setMovie] = useState([]);
+    const [movieRate, setMovieRate] = useState([]);
 
     useEffect(() => {
         const fetchMovie = async () => {
@@ -15,13 +16,21 @@ function App() {
                     Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
                 },
             };
-            const url =
+            const url1 =
                 "https://api.themoviedb.org/3/movie/popular?language=vi&page=1";
+            const url2 =
+                "https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1";
 
-            const response = await fetch(url, options);
-            const data = await response.json();
+            const [res1, res2] = await Promise.all([
+                fetch(url1, options),
+                fetch(url2, options),
+            ]);
 
-            console.log(data);
+            const data1 = await res1.json();
+            const data2 = await res2.json();
+
+            setMovie(data1.results);
+            setMovieRate(data2.results);
         };
 
         fetchMovie();
@@ -32,8 +41,8 @@ function App() {
             <div className="bg-black pb-10">
                 <Header />
                 <Banner />
-                <MovieList title={"Phim Hot"} />
-                <MovieList title={"Phim Đề Cử"} />
+                <MovieList title={"Phim Hot"} data={movie} />
+                <MovieList title={"Phim Đề Cử"} data={movieRate} />
             </div>
         </>
     );
