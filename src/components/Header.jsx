073fd9902485627fc45f46047ab0c13c863c9e3c
VU DUY HAI <duyhai121120@gmail.com>
2024-11-16 03:47:16 +0900

    Update components MovieSearch Final + BUG?

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import PropType from 'prop-types'; 
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Header({ onSearch }) {
+    const [textSearch, setSearch] = useState(''); // Sửa lỗi useState
+
     return (
         <div className="p-4 bg-black flex items-center justify-between">
             <div className="flex items-center space-x-4">
@@ -24,10 +27,12 @@ function Header({ onSearch }) {
                     type="text"
                     placeholder="Search"
                     className="p-3 text-black"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={textSearch}
                 />
                 <button
                     className="p-2 text-white bg-red-600"
-                    onClick={() => onSearch('hit')}
+                    onClick={() => onSearch(textSearch)}
                 >
                     Search
                 </button>
@@ -37,7 +42,7 @@ function Header({ onSearch }) {
 }
 
 Header.propTypes = {
-    onSearch: PropType.func,
+    onSearch: PropTypes.func, // Sửa lỗi PropType
 };
 
 export default Header;
